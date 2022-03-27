2ba7cc0a7c8e2faf74a08b02a487297a3a51d328
ricardomouraspl <ricardo.moura@superlogica.com>
2022-03-27 17:00:09 -0300

    phone infromation

---
+++ app/Services/PhoneInformation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+class PhoneInformation
+{
+    public function getCountry(string $phoneNumber)
+    {
+        
+    }
+
+    // public function getState(string $phoneNumber);
+
+    // public function getCountryCode(string $phoneNumber);
+
+    // public function getNumber(string $phoneNumber);
+}
