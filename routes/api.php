9c7078f83b743ca108dd86ec09cfb62f51d60b48
ricardomouraspl <ricardo.moura@superlogica.com>
2022-03-30 16:33:43 -0300

    api endpoint name change

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('/phone-numbers')->group(function () {
+Route::prefix('/phones')->group(function () {
     Route::get('/', [PhoneController::class, 'list']);
 });
