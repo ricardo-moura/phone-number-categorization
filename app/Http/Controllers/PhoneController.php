a43d0c5c3e6b7ae6ed26b704a227e6241f37056e
ricardomouraspl <ricardo.moura@superlogica.com>
2022-03-30 16:33:51 -0300

    controller improvements

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\PhoneService;
+use Illuminate\Http\Request;
 use Exception;
 
 
@@ -15,11 +16,11 @@ class PhoneController extends Controller
         $this->phoneService = $phoneService;
     }
 
-    public function list()
+    public function list(Request $request)
     {
         try {
             $result = ['status' => 200];
-            $result['data'] = $this->phoneService->getAll();
+            $result['data'] = $this->phoneService->getAll($request);
         } catch (Exception $e) {
             $result = [
                 'status' => 500,
